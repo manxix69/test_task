4aad38baa334e045bdd6e71c7c0fbb19c93a64a1
manxix69 <manxix69@gmail.com>
2024-08-04 18:59:24 +0500

    task done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,30 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
+/*
+*    Задача
+* - Дан массив строк, необходимо удалить из него все дубли
+*   , оставшиеся строки объединить в одну в порядке следования в массиве.
+*/
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        String[] strings = {"строка1", "строка1","строка1",
+                "строка2","строка7","строка3",
+                "строка2","строка7","строка4",
+                "строка3","строка7","строка4",
+        };
 
+        Set<String> set = new LinkedHashSet<String>();
+        for (int i = 0; i < strings.length; i++) {
+            set.add(strings[i]);
+        }
 
+        strings = set.toArray(new String[set.size()]);
 
-
+        System.out.println(Arrays.toString(strings));
     }
-
 }
